b6bd4a2bdc806176c4a20e343307d4a337710430
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-06-18 18:34:33 +0200

    product labels

---
+++ ab_mrp/models/product_product.py
@@ -0,0 +1,15 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    printed_lot = fields.Many2one('stock.production.lot', string='Lote', store=False)
+    printed_expiration = fields.Date('Caducidad', store=False)
+
+
+
+
