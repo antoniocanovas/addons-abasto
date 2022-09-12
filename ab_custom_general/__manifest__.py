92d8bb3ec11fc9dd6d16477a13f46959ba2f7b70
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-12 17:30:56 +0200

    AB General module

---
+++ ab_custom_general/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'AB Custom General',
+    'version': '14.0.1.0.0',
+    'category': '',
+    'description': u"""
+    Personalizaciones varias AB
+""",
+    'author': 'Serincloud',
+    'depends': [
+        'account',
+        'ab_custom_picking',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+}
