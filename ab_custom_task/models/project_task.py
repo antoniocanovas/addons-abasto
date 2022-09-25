8fee2cf85fdf2431e366a9283ccd745c1ab1e94f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-26 00:20:00 +0200

    AB task

--- ab_custom_task/models/project_task.py
+++ ab_custom_task/models/project_task.py
@@ -46,7 +46,6 @@ class ProjectTask(models.Model):
                 self.env['account.analytic.line'].create({'name': record.name,
                                                           'account_id': record.project_id.analytic_account_id.id,
                                                           'task_id': record.id,
-                                                          'set_start_stop': True,
                                                           'employee_id': record.user_id.employee_id.id,
                                                           'product_uom_id': hour_uom.id,
                                                           'user_id': record.user_id.id,
