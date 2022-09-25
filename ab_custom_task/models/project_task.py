3fd626b3c5bd06efb4cefc73319c79dd57e2f561
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-25 17:42:52 +0200

    AB task

--- ab_custom_task/models/project_task.py
+++ ab_custom_task/models/project_task.py
@@ -14,11 +14,11 @@ class ProjectTask(models.Model):
     @api.depends('timesheet_ids.date_time', 'timesheet_ids.date_time_end')
     def get_ab_stage(self):
         for record in self:
-            hidden = True
+            started = False
             for li in record.timesheet_ids:
                 if li.date_time and not li.date_time_end:
-                    hidden = False
-        record.ab_stage = hidden
+                    started = False
+        record.ab_started = started
 
     ab_stage = fields.Boolean('ab_stage', store=True, compute=get_ab_stage)
 
