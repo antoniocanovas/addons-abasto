7403facfd540478e808eb7c421330f640f4a82d2
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-09-25 17:17:10 +0200

    AB task

--- ab_custom_task/models/project_task.py
+++ ab_custom_task/models/project_task.py
@@ -11,6 +11,16 @@ class ProjectTask(models.Model):
 
     is_closed = fields.Boolean(store=True)
 
+    @api_depends('timesheet_ids.date_time', 'timesheet_ids.date_time_end')
+    def get_ab_stage(self):
+        hidden = True
+        for li in record.timesheet_ids:
+            if li.date_time and not li.date_time_end:
+                hidden = False
+        record.ab_stage = hidden
+
+    ab_stage = fields.Boolean('ab_stage', store=True, compute=get_ab_stage)
+
     def button_task_done(self):
         for record in self:
             state_done = self.env['project.task.type'].search([('is_closed','=',True),('id','in',record.project_id.type_ids.ids)])[0]
