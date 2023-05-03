f86ba0a6f3634bb004a8f8415fc21b179769ec23
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-03 19:21:05 +0200

    ab_custom_mrp_v0

---
+++ ab_custom_mrp/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 IC - Pedro Guirao
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "AB Custom MRP",
+    "summary": "",
+    "version": "14.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Serincloud",
+    "website": "https://ingenieriacloud.com",
+    "license": "AGPL-3",
+    "depends": ["mrp_bom_group"],
+    "data": ["views/mrp_view.xml",
+             "data/default_rules.xml",
+#             "views/report_simple_label.xml"
+             ],
+    "installable": True,
+}
---
+++ ab_custom_mrp/models/__init__.py
@@ -0,0 +1,3 @@
+#from . import mrp_production
+#from . import account_analytic_line
+#from . import product_product
