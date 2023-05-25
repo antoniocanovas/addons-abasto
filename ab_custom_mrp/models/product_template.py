6605bcfa5f111d981a22615868c15e7d24d19f1b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-25 18:26:23 +0200

    mrp reg sanitario

--- ab_custom_mrp/models/product_template.py
+++ ab_custom_mrp/models/product_template.py
@@ -1,7 +1,17 @@
 # Copyright 2021 IC - Pedro Guirao
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+try:
+    import qrcode
+except ImportError:
+    qrcode = None
+try:
+    import base64
+except ImportError:
+    base64 = None
+from io import BytesIO
 
-from odoo import fields, models
+from odoo import models, fields, api, _, SUPERUSER_ID
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
@@ -14,4 +24,35 @@ class ProductTemplate(models.Model):
     conservacion = fields.Char('Conservacion', store=True)
     qr_url = fields.Char('Url QR', store=True)
 
+    @api.depends('qr_url')
+    def generate_qr(self):
+        if qrcode and base64:
+#            if not self.sequence:
+#                prefix = str(self.env['ir.config_parameter'].sudo().get_param(
+#                    'customer_product_qr.config.customer_prefix'))
+#                if not prefix:
+#                    raise UserError(
+#                        _('Set A Customer Prefix In General Settings'))
+#                self.sequence = prefix + self.env['ir.sequence'].next_by_code(
+#                    'res.partner') or '/'
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(self.qr_url)
+            qr.make(fit=True)
 
+            img = qr.make_image()
+            temp = BytesIO()
+            img.save(temp, format="PNG")
+            qr_image = base64.b64encode(temp.getvalue())
+            self.write({'qr': qr_image})
+#            return self.env.ref(
+#                'customer_product_qrcode.print_qr').report_action(self, data={
+#                'data': self.id, 'type': 'cust'})
+        else:
+            raise UserError(
+                _('Necessary Requirements To Run This Operation Is Not Satisfied'))
+    qr_image = fields.Binary('QR Code', store=True)
