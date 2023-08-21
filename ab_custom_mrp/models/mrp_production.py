8b09d7b7407e0f5da53900d537603af6a8562104
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-21 17:15:52 +0200

    elaboration date

--- ab_custom_mrp/models/mrp_production.py
+++ ab_custom_mrp/models/mrp_production.py
@@ -1,8 +1,7 @@
 # Copyright 2021 IC - Pedro Guirao
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models, api, _,
-
+from odoo import models, fields, api, _
 
 class MrpBom(models.Model):
     _inherit = "mrp.production"
