fa0b6d580821850412698483e1bc31272e5e090a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-08-21 18:04:22 +0200

    elaboration date

--- ab_custom_mrp/models/mrp_production.py
+++ ab_custom_mrp/models/mrp_production.py
@@ -7,18 +7,17 @@ from odoo.exceptions import UserError
 class MrpBom(models.Model):
     _inherit = "mrp.production"
 
-#    timesheet_ids = fields.One2many('account.analytic.line', 'mrp_id', string='Timesheets')
+    #    timesheet_ids = fields.One2many('account.analytic.line', 'mrp_id', string='Timesheets')
     group_id = fields.Many2one('mrp.bom.group', string='Tipo de trabajo')
-#    bom_easy_id = fields.Many2one('mrp.bom', string='Receta')
+    #    bom_easy_id = fields.Many2one('mrp.bom', string='Receta')
     printed_expiration = fields.Date('Caducidad', store=True)
     # Nuevo campo para cambar fechas de SM de elaboraciones documentadas de forma tardía:
     elaboration_date = fields.Datetime('Fecha elaboración', store=True, copy=False)
 
     # No funciona, lo hago con AS manaul hasta que pueda dedicar más tiempo:
-    @api.onchange('write_date')
+    @api.onchange('elaboration_date')
     def update_mrp_stock_move_date(self):
         sms = self.env['stock.move'].search([('production_id','=',self.id)])
         for sm in sms:
-            if sm.date != record.elaboration_date:
-                sm['date'] = self.elaboration_date
+            sm['date'] = self.elaboration_date
 
